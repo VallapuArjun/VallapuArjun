c37792f163d81271c54f895d1515add8b8eddf9e
Vallapu arjun <136342339+VallapuArjun@users.noreply.github.com>
2023-08-07 07:24:50 +0000

    string.py

---
+++ python/Strings.py
@@ -0,0 +1,17 @@
+#String Concatenation
+a = "vallapu" + " " + "Arjun"
+print(a)
+# Output: vallapu Arjun
+
+#String Repetition
+a = "Arjun " * 5
+print(a)
+#Output:Arjun Arjun Arjun Arjun Arjun
+
+#Length of String
+name = input()
+length = len(name)
+print(length)
+#output:
+# arjun
+# 5
