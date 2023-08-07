c12a48a931837dbbe764bd38a917b373028f006b
Vallapu arjun <136342339+VallapuArjun@users.noreply.github.com>
2023-08-07 07:08:48 +0000

    intro.py

---
+++ python/intro.py
@@ -0,0 +1,44 @@
+#Why Python?
+#used for:
+#1. Web Development
+#2. Game Development
+#3. Artificial Intelligence and Machine Learning#
+
+#print statement in python
+print("Arjun")#output:Arjun
+
+#mistakes
+#- Spelling Mistakes:
+#prnt(”Hello World!”)
+#- Uppercase ‘P’:
+#Print(”Hello World!”)
+#- Missing quotes:
+#print(Hello World!)
+
+#calculations:
+print(2+5)
+#Output: 7
+print(2-5)
+#Output: -3
+print(2*5)
+#Output: 10
+print(6/3)
+#Output: 2.0
+
+#Assigning Variables
+a = "Arjun"
+b = "Loves"
+c = "Python"
+print(a+" "+b+" "+c)
+#Output: Arjun Loves Python
+a = 2
+b = 10
+print(a+b)
+#Output:12
+
+#Taking Input from User:
+a = input("Enter your name: ")
+print("Your name is "+a) 
+#Output:
+#Enter your name: vallapu arjun
+#Your name is vallapu arjun
